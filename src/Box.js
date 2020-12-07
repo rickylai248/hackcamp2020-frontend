507cab59fbda7ff4f1a9b6d3424f4f451dcdadec
rickylai <f5x2b@ugrad.cs.ubc.ca>
2020-12-06 17:19:03 -0800

    Changed box width to 30 , testing

--- src/Box.js
+++ src/Box.js
@@ -4,15 +4,14 @@ import MuiBox from "@material-ui/core/Box";
 const Box = () => {
   return (
     <MuiBox
-    width="100"
+    width="30"
     color="grey"
     bgcolor="grey.300"
     fontSize={{ xs: 'h6.fontSize', sm: 'h4.fontSize', md: 'h3.fontSize' }}
     p={{ xs: 2, sm: 3, md: 4 }}
     borderRadius="20%"
     borderRadius="borderRadius"
-    borderRadius={16}
-    >
+    borderRadius={16}>
       HappyHippo! TEXT
     </MuiBox>
   );
